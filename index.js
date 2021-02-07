8074854fb32e0cccae3f7c39d058d99dba53492f
moyshik7 <moyshik7@gmail.com>
2021-02-08 00:46:15 +0600

    added connecting

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const mongo = require("mongodb");
+const Client = mongo.MongoClient;
 class DB {
     constructor(input){
         if(!input){
 	    throw new Error("No input given");
 	}else{
 	    if(typeof(input) == "string"){
+		this.url = input;
+		Client.connect(url, function(err, db) {
+                    if (err) throw err;
+                    this.db = db;
+                });
 	    }else if(typeof(input) == "object"){
+		if(input.host && input.port && (input.db || input.dbName)){
+		}else{
+		    throw new Error("Invalid Input");
+		}
 	    }else{
 		throw new Error("Invalid input");
 	    }
 	}
     }
 }
+
+module.exports = DB;
